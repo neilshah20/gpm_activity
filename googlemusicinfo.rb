c6415af79cf6cce3d96d222872bcb09101e1d53a
Neil S <neilshah20@gmail.com>
2020-02-08 19:12:48 -0500

    Add files via upload

--- googlemusicinfo.rb
+++ googlemusicinfo.rb
@@ -2,6 +2,7 @@ require 'json'
 file = File.read('MyActivity.json')
 music_data = JSON.parse(file)
 
+# User input
 print "Enter a start year: "
 start_year = gets.to_i
 print "Enter an end year: "
@@ -21,7 +22,7 @@ for i in 1..(music_data.length() - 1)
   
   if song_year < start_year  # Stops loop from running if already past relevant data
     break
-  elsif (song_year <= end_year) && (song["title"].include? "Listened to") && (!(music_data[i - 1]["title"].include? "Skipped #{song_name}"))
+  elsif (song_year <= end_year) && (song["title"] =~ /Listened to/) && (!(music_data[i - 1]["title"].include? "Skipped #{song_name}"))
     # Runs only if the song is in the correct time frame and also was not immediately skipped
 
     artist = song["description"]
@@ -34,8 +35,6 @@ for i in 1..(music_data.length() - 1)
   end
 end
 
-
-
 puts "\n\n\n\n\nAssuming an average song time of 3 minutes, you listened to #{total_listen_time} minutes, or about #{total_listen_time / 60} hours, worth of music!\n\n\n"
 
 puts "Your #{num_to_display} most listened to songs were:"
@@ -44,7 +43,7 @@ times_listened.sort_by { |_, value| value }.each do |key, value|
   if counter >= num_to_display
     break
   else
-    puts("#{key} (#{-value})")
+    puts("#{key} (#{-value} listens)")
     counter += 1
   end
 end
@@ -55,7 +54,7 @@ artists_listened.sort_by { |_, value| value }.each do |key, value|
   if counter >= num_to_display
     break
   else
-    puts("#{key} (#{-value})")
+    puts("#{key} (#{-value} listens)")
     counter += 1
   end
 end
